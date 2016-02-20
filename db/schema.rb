db359cfb85938401fe39bc52828e436aa5f1cc73
Sanzhar Musaev <otakusan777@gmail.com>
2016-02-20 14:03:31 +0600

    Added Question model
    Generated Question model
    Added validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220061851) do
+ActiveRecord::Schema.define(version: 20160220080108) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string   "name"
