a0ce79530e7ff26b42295e442da65a9460c3ce48
Sanzhar Musaev <otakusan777@gmail.com>
2016-02-20 14:40:07 +0600

    Added Answer model
    Generated Answer model
    Added validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220080108) do
+ActiveRecord::Schema.define(version: 20160220083641) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.boolean  "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text     "content"
