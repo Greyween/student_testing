4681890efdbf6b95e5270da8249d771e7a2cf796
Sanzhar Musaev <otakusan777@gmail.com>
2016-02-20 13:59:02 +0600

    Corrected errors in code
    Changed User migration file
    Changed sanitization function in ApplicationController
    Changed Login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.for(:sign_in) << :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160220061851) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",              default: "", null: false
     t.string   "encrypted_password", default: "", null: false
     t.datetime "created_at",                      null: false
