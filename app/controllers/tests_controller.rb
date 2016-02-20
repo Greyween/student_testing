f220b761e23562b43040297c5539c571a4508554
Sanzhar Musaev <otakusan777@gmail.com>
2016-02-20 12:53:59 +0600

    Added TestController and views
    Generated TestController
    Added actions
    Changed routes.rb
    Added and changed views

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,52 @@
+class TestsController < ApplicationController
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      flash[:success] = 'Test has been successfully created'
+      redirect_to @test
+    else
+      flash[:alert] = 'Test has not been created'
+      redirect_to tests_url
+    end
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def update
+    @test = Test.find(params[:id])
+    if @test.update(test_params)
+      flash[:success] = 'Test has been successfully updated'
+      redirect_to @test
+    else
+      flash[:alert] = 'Test has not been updated'
+      redirect_to tests_url
+    end
+  end
+
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+    flash[:success] = 'Test has been successfully deleted'
+    redirect_to tests_url
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:name, :description)
+  end
+end
